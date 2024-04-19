d840ba23eeaaf967d0323e5d95abd4465872b502
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-04-19 15:12:17 -0300

    nsg fix

--- modules/vnet/distributor/main.tf
+++ modules/vnet/distributor/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_network_security_rule" "allow_inbound_sql_vnet" {
   source_port_range           = "*"
   destination_port_range      = "1433"
   source_address_prefix       = "VirtualNetwork"
-  destination_address_prefix  = "*"
+  destination_address_prefix  = "VirtualNetwork"
   resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.default.name
 }
